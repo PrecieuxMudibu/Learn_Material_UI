282d747f3ec85add32ac97e5e82dcc7c3dcb0389
PrecieuxMudibu <oceangpmb@gmail.com>
2023-03-08 23:06:15 +0100

    Add a checkbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from '@mui/material';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { useState } from "react";
 // import SaveIcon from "@mui/icons-material/SaveIcon"
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import {ButtonGroup} from '@mui/material';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { ButtonGroup } from "@mui/material";
+import {} from "@mui/material";
+import { DeleteForever } from "@mui/icons-material";
+// import {FormControlLabel} from "@mui/material";
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <div>
+      <FormControlLabel
+        control={
+         
+          <Checkbox
+            checked={checked}
+            checkedIcon={<DeleteForever />}
+            onChange={(e) => setChecked(e.target.checked)}
+            inputProps={{ "aria-label": "secondary checkbox" }}
+          />
+        }
+        label="Assertion 1"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
 
         <ButtonGroup>
-          <Button startIcon={<FavoriteIcon />} end Icon={<FavoriteIcon />} onClick={()=>alert("Hello")} href="#" variant="contained" color="primary">Discard</Button>
-          <Button startIcon={<FavoriteIcon />} end Icon={<FavoriteIcon />} onClick={()=>alert("Hello")} href="#" variant="contained" color="secondary">Save</Button>
+          <Button
+            startIcon={<FavoriteIcon />}
+            end
+            Icon={<FavoriteIcon />}
+            onClick={() => alert("Hello")}
+            href="#"
+            variant="contained"
+            color="primary"
+          >
+            Discard
+          </Button>
+          <Button
+            startIcon={<FavoriteIcon />}
+            end
+            Icon={<FavoriteIcon />}
+            onClick={() => alert("Hello")}
+            href="#"
+            variant="contained"
+            color="secondary"
+          >
+            Save
+          </Button>
         </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
     </div>
   );
