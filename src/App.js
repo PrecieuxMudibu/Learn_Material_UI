ca81d812d9ae3d53eb71a303337a2d54a50e7c69
PrecieuxMudibu <oceangpmb@gmail.com>
2023-03-08 22:40:01 +0100

    Add a button group component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
+// import SaveIcon from "@mui/icons-material/SaveIcon"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import {ButtonGroup} from '@mui/material';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={()=>alert("Hello")} href="#" variant="contained" color="primary">Hello World</Button>
+
+        <ButtonGroup>
+          <Button startIcon={<FavoriteIcon />} end Icon={<FavoriteIcon />} onClick={()=>alert("Hello")} href="#" variant="contained" color="primary">Discard</Button>
+          <Button startIcon={<FavoriteIcon />} end Icon={<FavoriteIcon />} onClick={()=>alert("Hello")} href="#" variant="contained" color="secondary">Save</Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
